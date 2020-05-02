863e548e21587d427643669e642d2e653d4d2d2b
sonquer <sonquer@o2.pl>
2020-05-02 23:50:48 +0200

    Cleaned ground class

--- src/game/core/Ground.ts
+++ src/game/core/Ground.ts
@@ -1,7 +1,6 @@
 import { EngineMediator } from "../utils/EngineMediator";
 import { IMapObject } from "./interfaces/IMapObject";
 import { IMapServerModel } from "../server/interfaces/serverModels/IMapServerModel";
-import { Character } from "./Character";
 import { singleton } from "tsyringe";
 import { AnimatedValue } from "../utils/AnimatedValue";
 import { Camera } from "./Camera";
@@ -50,63 +49,11 @@ export class Ground {
         });
     }
 
-    // public calculatedMapPosition() {
-    //     const character = this.character;
-        
-    //     let marginX = Math.round((window.innerWidth - this.widthInPixels) / 2);
-    //     let marginY = Math.round((window.innerHeight - this.heightInPixels) / 2);
-
-    //     if (marginX < 0) {
-    //         marginX = 0;
-    //     }
-
-    //     if (marginY < 0) {
-    //         marginY = 0;
-    //     }
-
-    //     let calculatedX = (window.innerWidth / 2) - (character.positionX * 32);
-    //     let calculatedY = (window.innerHeight / 2) - (character.positionY * 32);
-
-    //     const lockedLeft = calculatedX >= 0;
-    //     character.cameraLock.left = lockedLeft;
-    //     if (lockedLeft) {
-    //         calculatedX = marginX;
-    //     }
-
-    //     const lockedRight = calculatedX <= -this.widthInPixels + window.innerWidth - marginX;
-    //     character.cameraLock.right = lockedRight;
-    //     if (lockedRight) {
-    //         calculatedX = -this.widthInPixels + window.innerWidth - marginX;
-    //     }
-
-    //     const lockedTop = calculatedY >= marginY;
-    //     character.cameraLock.top = lockedTop;
-    //     if (lockedTop) {
-    //         calculatedY = marginY;
-    //     }
-
-    //     const lockedBottom = calculatedY <= -this.heightInPixels + window.innerHeight - marginY;
-    //     character.cameraLock.bottom = lockedBottom;
-    //     if (lockedBottom) {
-    //         calculatedY = -this.heightInPixels + window.innerHeight - marginY;
-    //     }
-
-    //     this.character.mapPositionX = Math.abs(Math.fround(calculatedX / 32));
-    //     this.character.mapPositionY = Math.abs(Math.fround(calculatedY / 32));
-
-    //     return { 
-    //         mapX: calculatedX, 
-    //         mapY: calculatedY 
-    //     };
-    // }
-
     public isLoaded() : boolean {
         return typeof(this.image) !== typeof(undefined);
     }
 
     public getGroundRenderObject() : IMapObject {
-        //const character = this.character;
-
         return {
             image: this.image as HTMLImageElement,
             dx: this.animatedPositionX,
